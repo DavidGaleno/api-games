23009b9a346cc708168128593782d0a9243fc0b7
David Galeno <davidgaleno411@gmail.com>
2023-12-03 18:07:39 -0300

    feat front-end

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -55,9 +55,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [    
-    "http://127.0.0.1:5500",]
+
 
 ROOT_URLCONF = 'setup.urls'
 
